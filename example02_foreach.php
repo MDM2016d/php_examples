82541079daa674839441794e7b9cc6cd86d6a575
Balthasar Hofer <lebalz@outlook.com>
2019-02-03 16:51:26 +0100

    add foreach example

---
+++ example02_foreach.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+  <h1>foreach</code></h1>
+  <p>Mit foreach</code> kann durch ein Array iteriert werden. </p>
+  <h2>Klassenliste</h2>
+  <ul>
+    <?php
+      $namen = ["Aaron", "David", "Elia", "Eliane", "Fabienne", "Fabio", "Felix", "Florian", "Florin", "Jan", "Janis", "Jona", "Julian", "Kerstin", "Marco", "Nick", "Nicolas"];
+      foreach($namen as $name) {
+        // in $name ist das aktuelle ELement des Arrays $namen gespeichert.
+        // Also zuerst 'Aaron', dann 'David',...
+        echo('<li>' . $name . '</li>');
+      }
+    ?>
+  </ul>
+
+  <h2>PHP Arrays: Schlüssel => Wert</code></h2>
+
+  <?php
+    // personen als Array von Arrays angeben:
+    // einzelne Werte im Array können als Assoziationen angegeben werden:
+    // im Feld 'name' ist  
+    $persons = [
+      ['name' => 'Muster', 'vorname' => 'Mark'],
+      ['name' => 'Smith', 'vorname' => 'Eleonore'],
+      ['name' => 'Einstein', 'vorname' => 'Albert']
+    ];
+  ?>
+  <table border="1">
+    <tr>
+      <th>Name</th>
+      <th>Vorname</th>
+    </tr>
+    <?php
+      foreach($persons as $person) {
+        // 1. Durchlauf: $person = ['name' => 'Muster', 'vorname' => 'Mark']
+        // 2. Durchlauf: $person = ['name' => 'Smith', 'vorname' => 'Eleonore']
+        // 3. Durchlauf: $person = ['name' => 'Einstein', 'vorname' => 'Albert']
+        echo('<tr>');
+          echo('<td>' . $person['name'] .'</td>');
+          echo('<td>' . $person['vorname'] .'</td>');
+        echo('</tr>');
+      }
+    ?>
+  </table>
+
+
+</body>
+
+</html>
