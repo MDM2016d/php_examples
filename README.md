27ca8251eff8ebb3ffca84b2c3adf14abeecb06a
Balthasar Hofer <lebalz@outlook.com>
2019-02-03 18:55:55 +0100

    add post example

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 * [01 Funktionen](./example01_funktionen.php) VErwendung von Funktionen in PHP Dokumenten
 * [02 `foreach`](./example02_foreach.php) Iterieren über Arrays
 * [03 Datenbankverbindung `MySQLi`](./example03_datenbankverbindung.php) Eine Verbindung zu einem MySQL Server herstellen
+* [04 POST](./example04_post/) hinzufügen eines Datensatzes zur Datenbank
---
+++ example04_post/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<header>
+</header>
+
+<body>
+  <h1>Formular mit Feldern zum Hinzufügen eines Datensatzes</h1>
+  <form action="addEntry.php" method="POST">
+    <label>Name</label>
+    <input name="name" type="text">
+    <br>
+    <label>Vorname</label>
+    <!-- "name" gibt an, wo der eingegebene Text gespeichert wird.
+     Hier bspw. steht im File 'addEntry.php' unter
+     $_POST['vorname'] dann der eingegeben text. -->
+    <input id="vorname" name="vorname" type="text">
+    <br>
+    <label>Lieblingszahl</label>
+    <input id="favorite_number" name="favorite_number" type="number">
+    <br>
+    <input type="submit" name="Speichern">
+  </form>
+  <!-- <?php echo($_GET['name']) ?> -->
+</body>
+
+</html>
