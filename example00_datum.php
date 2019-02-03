72b8988acc9d35c5aa22433af84db3fcb95fbd04
Balthasar Hofer <lebalz@outlook.com>
2019-02-03 16:08:53 +0100

    add datum example

---
+++ example00_datum.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+  <h1>Datum</h1>
+  <?php   // php skript tag
+    $datum = date("d.m.Y"); // aktuelles Datum
+    $time = date("H:i:s");
+    echo('Ich wurde mit PHP am <b>' . $datum . '</b> um ' . $time . ' Uhr erzeugt.');
+  ?>
+</body>
+
+</html>
